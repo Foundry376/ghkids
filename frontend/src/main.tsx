7ee123a4838d46e2a3390273e3ae0e791ff9c067
Ben Gotow <ben@foundry376.com>
2024-11-15 23:17:42 -0600

    Move to Vite

---
+++ frontend/src/main.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/default */
+
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { Router, browserHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+
+// Add support for touch-based click events
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// injectTapEventPlugin();
+
+// Apply various polyfills
+import "core-js";
+
+import routes from "./routes";
+import configureStore from "./store/configureStore";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.scss";
+import "./styles/font-awesome.min.css";
+import { createRoot } from "react-dom/client";
+
+const store = configureStore();
+window.store = store;
+
+// // Create an enhanced history that syncs navigation events with the store
+// const history = syncHistoryWithStore(browserHistory, store);
+
+createRoot(document.getElementById("root")!).render(<h2>hello</h2>);
