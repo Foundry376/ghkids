110d190aad30aa30995ef033db02431fa5ba6cc4
Ben Gotow <ben@foundry376.com>
2025-10-05 14:12:24 -0500

    Rework the rule sidebar CSS a bit

--- frontend/src/editor/components/inspector/content-flow-group.tsx
+++ frontend/src/editor/components/inspector/content-flow-group.tsx
@@ -71,25 +71,23 @@ export const ContentFlowGroup = ({
 
   return (
     <div>
-      <div className={`header ${rule.behavior}`}>
-        <div style={{ float: "left", width: 20 }}>
+      <div className={`${rule.behavior}`}>
+        <div style={{ display: "flex", gap: 4, alignItems: "baseline" }}>
           <RuleStateCircle rule={rule} />
+          <TapToEditLabel className="name" value={rule.name} onChange={_onNameChange} />
         </div>
-        <TapToEditLabel className="name" value={rule.name} onChange={_onNameChange} />
 
-        <div style={{ clear: "both" }}>
-          <div style={{ display: "inline-block", width: 16 }}>
-            <DisclosureTriangle
-              onClick={() => {
-                setCheckCollapsed(!checkCollapsed);
-                if (rule.check) {
-                  persistCollapsedState(rule.check.id, !collapsed);
-                }
-              }}
-              enabled={!!rule.check}
-              collapsed={!rule.check || checkCollapsed}
-            />
-          </div>
+        <div style={{ display: "flex", alignItems: "baseline", gap: 4 }}>
+          <DisclosureTriangle
+            onClick={() => {
+              setCheckCollapsed(!checkCollapsed);
+              if (rule.check) {
+                persistCollapsedState(rule.check.id, !collapsed);
+              }
+            }}
+            enabled={!!rule.check}
+            collapsed={!rule.check || checkCollapsed}
+          />
           <select
             value={rule.check ? "when" : "always"}
             onChange={(e) => {
@@ -107,53 +105,65 @@ export const ContentFlowGroup = ({
 
         {rule.check && !checkCollapsed && <ContentFlowGroupCheck check={rule.check} />}
 
-        <div style={{ paddingLeft: rule.check ? 20 : 0 }}>
-          <div style={{ display: "inline-block", width: 16 }}>
-            <DisclosureTriangle
-              onClick={() => {
-                setCollapsed(!collapsed);
-                persistCollapsedState(rule.id, !collapsed);
-              }}
-              collapsed={collapsed}
-            />
-          </div>
-          <select onChange={_onBehaviorChanged} value={rule.behavior}>
-            <option key={FLOW_BEHAVIORS.FIRST} value={FLOW_BEHAVIORS.FIRST}>
-              Do First Match
-            </option>
-            <option key={FLOW_BEHAVIORS.LOOP} value={FLOW_BEHAVIORS.LOOP}>
-              Do First Match &amp; Repeat
-            </option>
-            <option key={FLOW_BEHAVIORS.ALL} value={FLOW_BEHAVIORS.ALL}>
-              Do All &amp; Continue
-            </option>
-            <option key={FLOW_BEHAVIORS.RANDOM} value={FLOW_BEHAVIORS.RANDOM}>
-              Randomize &amp; Do First
-            </option>
-          </select>
-          {rule.behavior === FLOW_BEHAVIORS.LOOP ? (
-            <select
-              onChange={_onLoopCountChanged}
-              value={JSON.stringify(rule.loopCount) || `{"constant":2}`}
-            >
-              <option value={`{"constant":2}`}>2 Times</option>
-              <option value={`{"constant":3}`}>3 Times</option>
-              <option value={`{"constant":4}`}>4 Times</option>
-              <option value={`{"constant":5}`}>5 Times</option>
-              <option value={`{"constant":6}`}>6 Times</option>
-              <option value={`{"constant":7}`}>7 Times</option>
-              <option value={`{"constant":8}`}>8 Times</option>
-              <option value={`{"constant":9}`}>9 Times</option>
-              <option value={`{"constant":10}`}>10 Times</option>
-              <option disabled>_____</option>
-              {variables.map(({ id, name }) => (
-                <option value={`{"variableId":"${id}"}`} key={id}>
-                  "{name}" Times
-                </option>
-              ))}
-              {variables.length === 0 ? <option disabled>No variables defined</option> : undefined}
+        <div
+          style={{
+            paddingLeft: rule.check ? 16 : 0,
+            display: "flex",
+            alignItems: "baseline",
+            gap: 4,
+          }}
+        >
+          <DisclosureTriangle
+            collapsed={collapsed}
+            onClick={() => {
+              setCollapsed(!collapsed);
+              persistCollapsedState(rule.id, !collapsed);
+            }}
+          />
+          <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+            <select onChange={_onBehaviorChanged} value={rule.behavior}>
+              <option key={FLOW_BEHAVIORS.FIRST} value={FLOW_BEHAVIORS.FIRST}>
+                Do First Match
+              </option>
+              <option key={FLOW_BEHAVIORS.LOOP} value={FLOW_BEHAVIORS.LOOP}>
+                Do First Match &amp; Repeat
+              </option>
+              <option key={FLOW_BEHAVIORS.ALL} value={FLOW_BEHAVIORS.ALL}>
+                Do All &amp; Continue
+              </option>
+              <option key={FLOW_BEHAVIORS.RANDOM} value={FLOW_BEHAVIORS.RANDOM}>
+                Randomize &amp; Do First
+              </option>
             </select>
-          ) : undefined}
+            {rule.behavior === FLOW_BEHAVIORS.LOOP ? (
+              <select
+                onChange={_onLoopCountChanged}
+                value={JSON.stringify(rule.loopCount) || `{"constant":2}`}
+              >
+                <option value={`{"constant":2}`}>2 Times</option>
+                <option value={`{"constant":3}`}>3 Times</option>
+                <option value={`{"constant":4}`}>4 Times</option>
+                <option value={`{"constant":5}`}>5 Times</option>
+                <option value={`{"constant":6}`}>6 Times</option>
+                <option value={`{"constant":7}`}>7 Times</option>
+                <option value={`{"constant":8}`}>8 Times</option>
+                <option value={`{"constant":9}`}>9 Times</option>
+                <option value={`{"constant":10}`}>10 Times</option>
+                <option disabled>_____</option>
+                {variables.map(({ id, name }) => (
+                  <option value={`{"variableId":"${id}"}`} key={id}>
+                    "{name}" Times
+                  </option>
+                ))}
+                {variables.length === 0 ? (
+                  <option disabled>No variables defined</option>
+                ) : undefined}
+              </select>
+            ) : undefined}
+          </div>
+        </div>
+
+        <div style={{ paddingLeft: rule.check ? 16 : 0 }}>
           <RuleList
             parentId={rule.id}
             rules={rule.rules}
